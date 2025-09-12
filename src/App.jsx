ba74339986a292517250c7269cc12193edc2b202
Spandan Shah <spandanshah1503@gmail.com>
2025-09-12 20:59:12 +0530

    Setting up background as DarkVeil

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import DarkVeil from "./components/DarkVeil"
 
-function App() {
+function App() 
+{
 
-  return (
+  return 
+  (
     <>
-    <h1>Portfolio</h1>
+
+       <div style={{ width: "100%", height: "100vh", position: "relative" }}>
+      <DarkVeil 
+        hueShift={0}
+        noiseIntensity={0.15}
+        scanlineIntensity={0.2}
+        scanlineFrequency={2}
+        warpAmount={0.1}
+        speed={0.7}
+      />
+      <h1 style={{
+        position: "absolute",
+        top: "40%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        color: "white",
+        zIndex: 10
+      }}>
+        🚀 My Portfolio
+      </h1>
+    </div>
+
+  
+     
+     <DarkVeil />
     </>
   )
 }
