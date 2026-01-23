37f06297230b5b590f7785cf70b011a9058e9973
Spandan Shah <spandanshah1503@gmail.com>
2026-01-23 22:35:03 +0530

    feat(styling): define main layout section with responsive styling

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 
 const Work = () => {
   return (
-    <div>
-      
+    <>
+    
+    <div className="">
+      <Navbar />       
     </div>
+
+    <main className="flex-1 px-4 sm::px-6 lg:px-8 mt-6"> 
+      <section className="Description text-center ">
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl text-white ">
+          Work & Projects
+        </h1>
+      </section>
+
+
+    </main>
+
+
+
+    <div className = "mt-6 ">
+      <Footer />
+    </div>
+    
+    </>
   );
 };
 
