e40cc84341fc1a3b7eda87244b0a6dcfe0f43f3d
Spandan Shah <spandanshah1503@gmail.com>
2026-01-24 14:27:35 +0530

    chore(stying): Defining the layout of Work Section

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -11,12 +11,16 @@ const Work = () => {
     </div>
 
     <main className="flex-1 px-4 sm::px-6 lg:px-8 mt-6"> 
+      
       <section className="Description text-center ">
         <h1 className="text-3xl sm:text-4xl lg:text-5xl text-white ">
           Work & Projects
         </h1>
       </section>
-
+      <section id="Work" className="mt-10 min-h-screen mb-40">
+        <h2 className="text-2xl text-white">Work</h2>
+        <p className="text-white">This is a collection of my professional work, highlighting hands-on experience, responsibilities, and contributions to real products in collaborative and production environments.</p>
+      </section>
 
     </main>
 
